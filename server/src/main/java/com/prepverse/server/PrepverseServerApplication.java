6494d1a514b86040e26c583993c028c8b9371088
Shreyash Kale <shreyash07kale@gmail.com>
2026-07-12 02:00:18 +0530

    chore: initialize PrepVerse backend with Spring Boot

---
+++ server/src/main/java/com/prepverse/server/PrepverseServerApplication.java
@@ -0,0 +1,13 @@
+package com.prepverse.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepverseServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrepverseServerApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/prepverse/server/PrepverseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prepverse.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepverseServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
